f3c263515350f47fecdcd525e7be19ca930d906c
felipefrancca <suporte.felipefranca@gmail.com>
2023-11-10 06:15:27 -0300

    up

--- src/assets/scripts/draganddrop.js
+++ src/assets/scripts/draganddrop.js
@@ -8,27 +8,29 @@ document.addEventListener('dragend', (e) => {
     e.target.classList.remove('dragging');
 });
 
-columns.forEach((itens) => {
-    itens.addEventListener('dragover', (e) => {
+columns.forEach((item) => {
+    item.addEventListener('dragover', (e) => {
         const dragging = document.querySelector('.dragging');
-        const applyAfter = getNewPosition(itens, e.clientY);
+        const applyAfter = getNewPosition(item, e.clientY);
 
         if (applyAfter) {
             applyAfter.insertAdjacentElment('afterend', dragging);
         } else {
-            itens.prepend(dragging);
+            item.prepend(dragging);
         }
     });
 });
 
 function getNewPosition(column, posY) {
-    const cards = column.querySelectorAll('.itens:not(.dragging)');
+    const cards = column.querySelectorAll('.item:not(.dragging)');
     let result;
 
     for (let refer_card of cards) {
-        const box = refer_card.getBoundingClienteReact();
+        const box = refer_card.getBoundingClientRect();
         const boxCenterY = box.y + box.height / 2;
 
         if (posY >= boxCenterY) result = refer_card;
     }
+
+    return result;
 }
